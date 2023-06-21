6f4c5a777fabc9707f2f91489d244b4cd797f837
dedottag <chponga@mail.ru>
2023-06-21 12:31:23 +0300

    1.6.1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { format, addYears } from "date-fns";
+n;
 import "./app.css";
 import MovieList from "../movie-list";
 import Footer from "../footer";
